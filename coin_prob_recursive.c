e8d5cffdc833fd01ff323cacbc9dc01627927099
Devashi Tandon <dt2450@github.com>
2020-11-20 00:11:14 +0530

    Recursive approach

--- coin_prob_recursive.c
+++ coin_prob_recursive.c
@@ -34,9 +34,16 @@ int main() {
     for(int c_i = 0; c_i < m; c_i++){
        scanf("%ld",&c[c_i]);
     }
+    
+    printf("Available coin denominations: \n");
+    for(int i = 0; i < m; i++) {
+        printf("%ld ", c[i]);
+    }
+    printf("\n");
+    
     // Print the number of ways of making change for 'n' units using coins having the values given by 'c'
     unsigned long long ways = getWays(n, m, c);
-    printf("%llu\n", ways);
+    printf("Number of ways of making change for %d cents is %llu\n", n, ways);
     
     return 0;
 }
